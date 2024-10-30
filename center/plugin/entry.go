b97defd357e64eb27ec4510d678d3f1d53f8d33b
sea <seazhu831@gmail.com>
2024-10-30 16:51:00 +0800

    fix: Register scriptman

--- center/plugin/entry.go
+++ center/plugin/entry.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"github.com/sensdata/idb/center/core/conn"
 	"github.com/sensdata/idb/center/plugin/fileman/backend/fileman"
+	"github.com/sensdata/idb/center/plugin/script/backend/scriptman"
 	"github.com/sensdata/idb/center/plugin/ssh/backend/sshman"
 	"github.com/sensdata/idb/center/plugin/sysinfo/backend/sysinfo"
 	"github.com/sensdata/idb/center/plugin/systemctl"
@@ -10,13 +11,15 @@ import (
 
 func RegisterPlugins() {
 	// 注册sysinfo
-	conn.RegisterIdbPlugin(&sysinfo.SysInfo{})
+	conn.RegisterIdbPlugin(&sysinfo.Plugin)
 	// 注册files
-	conn.RegisterIdbPlugin(&fileman.FileMan{})
+	conn.RegisterIdbPlugin(&fileman.Plugin)
 	// 注册ssh
-	conn.RegisterIdbPlugin(&sshman.SSHMan{})
+	conn.RegisterIdbPlugin(&sshman.Plugin)
 	// 注册sysctl
-	conn.RegisterIdbPlugin(&systemctl.SystemCtl{})
+	conn.RegisterIdbPlugin(&systemctl.Plugin)
+	// 注册scripts
+	conn.RegisterIdbPlugin(&scriptman.Plugin)
 
 	// 执行所有模块的初始化
 	conn.InitializePlugins()
