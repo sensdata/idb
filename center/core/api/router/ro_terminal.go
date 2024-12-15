ad7fe5823698d55ea86e6b8a4e75a3177602ced9
sea <seazhu831@gmail.com>
2024-12-15 09:23:17 +0800

    fix: Temporarily remove authentication in WebSocket API

--- center/core/api/router/ro_terminal.go
+++ center/core/api/router/ro_terminal.go
@@ -3,14 +3,13 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/sensdata/idb/center/core/api/entry"
-	"github.com/sensdata/idb/center/core/api/middleware"
 )
 
 type TerminalRouter struct{}
 
 func (s *TerminalRouter) InitRouter(Router *gin.RouterGroup) {
 	terminalRouter := Router.Group("ws")
-	terminalRouter.Use(middleware.NewJWT().JWTAuth())
+	// terminalRouter.Use(middleware.NewJWT().JWTAuth())
 	baseApi := entry.ApiGroup
 	{
 		terminalRouter.GET("/terminals", baseApi.HandleTerminal) // WebSocket终端会话
