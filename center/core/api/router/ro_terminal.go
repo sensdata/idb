8aee458d81c1bbadcb79014f1b6cae88c4a9349f
sea <seazhu831@gmail.com>
2025-01-07 10:19:35 +0800

    feat: Add JWTCookieAuth middleware for JWT validation from cookies in terminal router

--- center/core/api/router/ro_terminal.go
+++ center/core/api/router/ro_terminal.go
@@ -10,19 +10,18 @@ type TerminalRouter struct{}
 
 func (s *TerminalRouter) InitRouter(Router *gin.RouterGroup) {
 	terminalRouter := Router.Group("terminals")
-	terminalRouter.Use(middleware.NewJWT().JWTCookieAuth())
 	baseApi := entry.ApiGroup
 	{
 		// websocket接口
-		terminalRouter.GET("/:host/ssh/start", baseApi.HandleSshTerminal) // 创建ssh终端会话
+		terminalRouter.GET("/:host/ssh/start", middleware.NewJWT().JWTCookieAuth(), baseApi.HandleSshTerminal) // 创建ssh终端会话
 
-		terminalRouter.GET("/:host/start", baseApi.HandleTerminal) // 创建或连接到终端会话
+		terminalRouter.GET("/:host/start", middleware.NewJWT().JWTCookieAuth(), baseApi.HandleTerminal) // 创建或连接到终端会话
 
 		// http接口
-		terminalRouter.GET("/:host/sessions", baseApi.TerminalSessions)     // 枚举终端会话
-		terminalRouter.POST("/:host/session/detach", baseApi.DetachSession) // 分离终端会话
-		terminalRouter.POST("/:host/session/quit", baseApi.QuitSession)     //终止终端会话
-		terminalRouter.POST("/:host/session/rename", baseApi.RenameSession) // 重命名终端会话
-		terminalRouter.POST("/:host/install", baseApi.InstallTerminal)      // 安装Agent侧的终端环境
+		terminalRouter.GET("/:host/sessions", middleware.NewJWT().JWTAuth(), baseApi.TerminalSessions)     // 枚举终端会话
+		terminalRouter.POST("/:host/session/detach", middleware.NewJWT().JWTAuth(), baseApi.DetachSession) // 分离终端会话
+		terminalRouter.POST("/:host/session/quit", middleware.NewJWT().JWTAuth(), baseApi.QuitSession)     //终止终端会话
+		terminalRouter.POST("/:host/session/rename", middleware.NewJWT().JWTAuth(), baseApi.RenameSession) // 重命名终端会话
+		terminalRouter.POST("/:host/install", middleware.NewJWT().JWTAuth(), baseApi.InstallTerminal)      // 安装Agent侧的终端环境
 	}
 }
