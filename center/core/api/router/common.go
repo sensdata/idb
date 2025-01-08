0b5513025c3c854995da04997dfe5299c830f548
sea <seazhu831@gmail.com>
2025-01-08 12:27:51 +0800

    feat: Add SettingsRouter for managing settings API endpoints

--- center/core/api/router/common.go
+++ center/core/api/router/common.go
@@ -18,5 +18,6 @@ func commonGroups() []CommonRouter {
 		&ActionRouter{},
 		&TerminalRouter{},
 		&AppRouter{},
+		&SettingsRouter{},
 	}
 }
---
+++ center/core/api/router/ro_settings.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sensdata/idb/center/core/api/entry"
+	"github.com/sensdata/idb/center/core/api/middleware"
+)
+
+type SettingsRouter struct{}
+
+func (s *SettingsRouter) InitRouter(Router *gin.RouterGroup) {
+	settingsRouter := Router.Group("settings")
+	settingsRouter.Use(middleware.NewJWT().JWTAuth())
+	baseApi := entry.ApiGroup
+	{
+		settingsRouter.GET("/about", baseApi.About)
+		settingsRouter.GET("", baseApi.Settings)
+		settingsRouter.POST("", baseApi.UpdateSettings)
+	}
+}
