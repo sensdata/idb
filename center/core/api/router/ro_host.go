11efb585cbd3a261ead82d7e63974052cdf2429f
sea <seazhu831@gmail.com>
2025-02-11 23:23:05 +0800

    fix: Add entrypoint for installing agent

--- center/core/api/router/ro_host.go
+++ center/core/api/router/ro_host.go
@@ -13,13 +13,14 @@ func (s *HostRouter) InitRouter(Router *gin.RouterGroup) {
 	hostRouter.Use(middleware.NewJWT().JWTAuth())
 	baseApi := entry.ApiGroup
 	{
-		hostRouter.GET("/groups", baseApi.ListHostGroup)            // 获取设备组列表
-		hostRouter.GET("", baseApi.ListHost)                        // 获取设备列表
-		hostRouter.POST("", baseApi.CreateHost)                     // 新增设备
-		hostRouter.PUT("", baseApi.UpdateHost)                      // 更新设备
-		hostRouter.PUT("/:host/ssh", baseApi.UpdateHostSSH)         // 更新设备ssh配置
-		hostRouter.PUT("/:host/agent", baseApi.UpdateHostAgent)     // 更新设备agent配置
-		hostRouter.POST("/:host/test/ssh", baseApi.TestHostSSH)     // 测试设备ssh
-		hostRouter.POST("/:host/test/agent", baseApi.TestHostAgent) // 测试设备agent
+		hostRouter.GET("/groups", baseApi.ListHostGroup)              // 获取设备组列表
+		hostRouter.GET("", baseApi.ListHost)                          // 获取设备列表
+		hostRouter.POST("", baseApi.CreateHost)                       // 新增设备
+		hostRouter.PUT("", baseApi.UpdateHost)                        // 更新设备
+		hostRouter.PUT("/:host/ssh", baseApi.UpdateHostSSH)           // 更新设备ssh配置
+		hostRouter.PUT("/:host/agent", baseApi.UpdateHostAgent)       // 更新设备agent配置
+		hostRouter.POST("/:host/test/ssh", baseApi.TestHostSSH)       // 测试设备ssh
+		hostRouter.POST("/:host/test/agent", baseApi.TestHostAgent)   // 测试设备agent
+		hostRouter.POST("/:host/install/agent", baseApi.InstallAgent) // 安装agent
 	}
 }
