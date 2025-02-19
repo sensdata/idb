9f5eeab3cdfd7f2ca6ab29517cb7baaf7bcecc41
sea <seazhu831@gmail.com>
2025-02-19 14:11:54 +0800

    ref: Add uninstall agent endpoint to host router

--- center/core/api/router/ro_host.go
+++ center/core/api/router/ro_host.go
@@ -13,19 +13,20 @@ func (s *HostRouter) InitRouter(Router *gin.RouterGroup) {
 	hostRouter.Use(middleware.NewJWT().JWTAuth())
 	baseApi := entry.ApiGroup
 	{
-		hostRouter.GET("/groups", baseApi.ListHostGroup)              // 获取设备组列表
-		hostRouter.GET("", baseApi.ListHost)                          // 获取设备列表
-		hostRouter.POST("", baseApi.CreateHost)                       // 新增设备
-		hostRouter.PUT("", baseApi.UpdateHost)                        // 更新设备
-		hostRouter.DELETE("", baseApi.DeleteHost)                     // 删除设备
-		hostRouter.GET("/:host/status", baseApi.HostStatus)           // 设备状态
-		hostRouter.GET("/:host", baseApi.HostInfo)                    // 设备配置信息
-		hostRouter.PUT("/:host/conf/ssh", baseApi.UpdateHostSSH)      // 更新设备ssh配置
-		hostRouter.PUT("/:host/conf/agent", baseApi.UpdateHostAgent)  // 更新设备agent配置
-		hostRouter.POST("/test/ssh", baseApi.TestHostSSH)             // 测试设备ssh
-		hostRouter.POST("/:host/test/agent", baseApi.TestHostAgent)   // 测试设备agent
-		hostRouter.POST("/:host/agent/install", baseApi.InstallAgent) // 安装agent
-		hostRouter.GET("/:host/agent/status", baseApi.AgentStatus)    // 获取agent状态
-		hostRouter.POST("/:host/agent/restart", baseApi.RestartAgent) // 重启agent
+		hostRouter.GET("/groups", baseApi.ListHostGroup)                  // 获取设备组列表
+		hostRouter.GET("", baseApi.ListHost)                              // 获取设备列表
+		hostRouter.POST("", baseApi.CreateHost)                           // 新增设备
+		hostRouter.PUT("", baseApi.UpdateHost)                            // 更新设备
+		hostRouter.DELETE("", baseApi.DeleteHost)                         // 删除设备
+		hostRouter.GET("/:host/status", baseApi.HostStatus)               // 设备状态
+		hostRouter.GET("/:host", baseApi.HostInfo)                        // 设备配置信息
+		hostRouter.PUT("/:host/conf/ssh", baseApi.UpdateHostSSH)          // 更新设备ssh配置
+		hostRouter.PUT("/:host/conf/agent", baseApi.UpdateHostAgent)      // 更新设备agent配置
+		hostRouter.POST("/test/ssh", baseApi.TestHostSSH)                 // 测试设备ssh
+		hostRouter.POST("/:host/test/agent", baseApi.TestHostAgent)       // 测试设备agent
+		hostRouter.POST("/:host/agent/install", baseApi.InstallAgent)     // 安装agent
+		hostRouter.POST("/:host/agent/uninstall", baseApi.UninstallAgent) // 卸载agent
+		hostRouter.GET("/:host/agent/status", baseApi.AgentStatus)        // 获取agent状态
+		hostRouter.POST("/:host/agent/restart", baseApi.RestartAgent)     // 重启agent
 	}
 }
