48820dedba8d83b613ad72790ca49ca85aa4bc46
sea <seazhu831@gmail.com>
2025-04-27 00:45:09 +0800

    fix: Fix logstream API route and offset configuration

--- core/logstream/pkg/reader/adapters/tail.go
+++ core/logstream/pkg/reader/adapters/tail.go
@@ -83,8 +83,8 @@ func (r *TailReader) Follow(offset int64, whence int) (<-chan []byte, error) {
 		MustExist: false,
 		Poll:      true,
 		Location: &tail.SeekInfo{
-			Offset: 0,
-			Whence: io.SeekStart,
+			Offset: offset,
+			Whence: whence,
 		},
 	}
 
