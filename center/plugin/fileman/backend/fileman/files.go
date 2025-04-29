6d237fee3c7db8882ff07cd23c23513a6016204c
sea <seazhu831@gmail.com>
2025-04-29 20:25:05 +0800

    fix: Update task status before sending SSE response

--- center/plugin/fileman/backend/fileman/files.go
+++ center/plugin/fileman/backend/fileman/files.go
@@ -508,6 +508,12 @@ func (s *FileMan) tailContentStream(c *gin.Context) error {
 	bufferCh := make(chan []byte, 100)
 	defer close(bufferCh)
 
+	// 更新一下任务状态
+	if err := global.LogStream.UpdateTaskStatus(task.ID, types.TaskStatusRunning); err != nil {
+		global.LOG.Error("Failed to update task status to %s : %v", types.TaskStatusRunning, err)
+		return fmt.Errorf("Failed to update task status to %s : %w", types.TaskStatusRunning, err)
+	}
+
 	// 设置 SSE 响应头
 	c.Writer.Header().Set("Content-Type", "text/event-stream")
 	c.Writer.Header().Set("Cache-Control", "no-cache")
