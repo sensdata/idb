c663fabbe22a0a122f10aedd0862f48b6c34a81c
sea <seazhu831@gmail.com>
2025-06-06 15:35:16 +0800

    refactor(sysctl): Refactor service operation API

--- center/plugin/systemctl/service.go
+++ center/plugin/systemctl/service.go
@@ -1,13 +1,17 @@
 package systemctl
 
 import (
+	"crypto/tls"
 	_ "embed"
+	"errors"
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
 	"github.com/sensdata/idb/center/core/api"
 	"github.com/sensdata/idb/center/core/api/service"
 	"github.com/sensdata/idb/center/global"
@@ -20,9 +24,9 @@ import (
 )
 
 type SystemCtl struct {
-	plugin     plugin.Plugin
-	pluginConf plugin.PluginConf
-	cmdHelper  *helper.CmdHelper
+	plugin      plugin.Plugin
+	pluginConf  plugin.PluginConf
+	restyClient *resty.Client
 }
 
 var LOG *log.Log
@@ -87,15 +91,34 @@ func (s *SystemCtl) Initialize() {
 	if settingInfo.BindDomain != "" && settingInfo.BindDomain != host {
 		host = settingInfo.BindDomain
 	}
+	baseUrl := fmt.Sprintf("%s://%s:%d/api/v1", scheme, host, settingInfo.BindPort)
 
-	s.cmdHelper = helper.NewCmdHelper(scheme, host, strconv.Itoa(settingInfo.BindPort), nil)
+	s.restyClient = resty.New().
+		SetBaseURL(baseUrl).
+		SetHeader("Content-Type", "application/json")
+
+	if settingInfo.Https == "yes" {
+		// 创建 TLS 配置
+		cert, err := tls.X509KeyPair(global.CertPem, global.KeyPem)
+		if err != nil {
+			global.LOG.Error("Failed to create cert: %v", err)
+			return
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates:       []tls.Certificate{cert}, // 设置服务器证书
+			MinVersion:         tls.VersionTLS13,        // 设置最小 TLS 版本
+			InsecureSkipVerify: true,
+		}
+		s.restyClient.SetTLSClientConfig(tlsConfig)
+	}
 
 	api.API.SetUpPluginRouters(
 		"sysctl",
 		[]plugin.PluginRoute{
 			{Method: "GET", Path: "/info", Handler: s.GetPluginInfo},
 			{Method: "GET", Path: "/menu", Handler: s.GetMenu},
-			{Method: "GET", Path: "/:host/boot", Handler: s.ServiceBoot},
+			{Method: "GET", Path: "/:host/operate", Handler: s.OperateService},
 		},
 	)
 
@@ -143,36 +166,82 @@ func (s *SystemCtl) getMenus() ([]plugin.MenuItem, error) {
 }
 
 // @Tags Sysctl
-// @Summary run systemctl start/stop/restart service commands
-// @Description 运行systemctl的服务启动相关命令
+// @Summary Operate service
+// @Description Performing operations on a systemd-managed service on the specified host. Supported actions include enabling/disabling on boot, starting, stopping, restarting, reloading, and get status of the service.
 // @Param host path uint true "Host ID"
-// @Success 200
-// @Router /sysctl/{host}/boot [post]
-func (s *SystemCtl) ServiceBoot(c *gin.Context) {
+// @Param request body model.OperateServiceReq true "Service operation"
+// @Success 200 {object} model.ServiceOperateResult
+// @Router /sysctl/{host}/operate [post]
+func (s *SystemCtl) OperateService(c *gin.Context) {
 	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
 	if err != nil {
 		helper.ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
 		return
 	}
 
-	var req model.ServiceBootReq
+	var req model.OperateServiceReq
 	if err := helper.CheckBindAndValidate(&req, c); err != nil {
 		return
 	}
 
-	err = s.serviceBoot(hostID, req)
+	result, err := s.serviceOperate(hostID, req)
 	if err != nil {
-		helper.ErrorWithDetail(c, constant.CodeErrInternalServer, constant.ErrInternalServer.Error(), err)
+		helper.ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
 		return
 	}
-	helper.SuccessWithData(c, nil)
+	helper.SuccessWithData(c, result)
 }
 
-func (s *SystemCtl) serviceBoot(hostID uint64, req model.ServiceBootReq) error {
-	_, err := s.cmdHelper.RunSystemCtl(uint(hostID), req.ServiceName, req.Command)
-	if err != nil {
-		LOG.Info("service boot err %v", err)
-		return err
+func (s *SystemCtl) serviceOperate(hostID uint64, req model.OperateServiceReq) (*model.ServiceOperateResult, error) {
+	var result model.ServiceOperateResult
+
+	var command string
+	switch req.Operation {
+	case "start", "stop", "restart", "reload", "enable", "disable":
+		command = fmt.Sprintf("systemctl %s %s", req.Operation, req.Service)
+	case "status":
+		command = fmt.Sprintf("systemctl show %s --property=ActiveState,SubState", req.Service)
+	default:
+		return &result, errors.New("unsupported operation")
 	}
-	return nil
+	commandResult, err := s.sendCommand(uint(hostID), command)
+	if err != nil {
+		LOG.Error("Failed to send service operation command")
+		return &result, err
+	}
+	LOG.Info("Service operation command result: %s", commandResult.Result)
+	result.Result = commandResult.Result
+	return &result, nil
+}
+
+func (s *SystemCtl) sendCommand(hostId uint, command string) (*model.CommandResult, error) {
+	var commandResult model.CommandResult
+
+	commandRequest := model.Command{
+		HostID:  hostId,
+		Command: command,
+	}
+
+	var commandResponse model.CommandResponse
+
+	resp, err := s.restyClient.R().
+		SetBody(commandRequest).
+		SetResult(&commandResponse).
+		Post("/commands")
+
+	if err != nil {
+		LOG.Error("failed to send request: %v", err)
+		return &commandResult, fmt.Errorf("failed to send request: %v", err)
+	}
+
+	if resp.StatusCode() != 200 {
+		LOG.Error("failed to send request: %v", err)
+		return &commandResult, fmt.Errorf("received error response: %s", resp.Status())
+	}
+
+	LOG.Info("cmd response: %v", commandResponse)
+
+	commandResult = commandResponse.Data
+
+	return &commandResult, nil
 }
--- core/model/sysctl.go
+++ core/model/sysctl.go
@@ -1,6 +1,6 @@
 package model
 
-type ServiceBootReq struct {
-	Command     string `json:"command"`
-	ServiceName string `json:"service_name"`
+type OperateServiceReq struct {
+	Service   string `json:"service"`
+	Operation string `json:"operation" validate:"required,oneof=start,stop,restart,enable,disable,reload,status"`
 }
