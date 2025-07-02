05c8c311afc87540e5a3f76331863335ec735d55
sea <seazhu831@gmail.com>
2025-07-02 14:34:19 +0800

    fix(utils): Fix password generation function

--- core/utils/nonce.go
+++ core/utils/nonce.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 	"io"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
-const passCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789^&-_="
+
+const (
+	lettersAndDigits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	symbols          = "^&-_=+"
+)
 
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+var randMu sync.Mutex
 
 func GenerateMsgId() string {
 	b := make([]byte, 8)
@@ -30,10 +36,32 @@ func GenerateNonce(length int) string {
 }
 
 func GeneratePassword(length int) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(passCharset))]
+	if length < 2 {
+		// 至少需要2位：1个符号 + 1个其他字符
+		return ""
 	}
+
+	randMu.Lock()
+	defer randMu.Unlock()
+
+	// 1. 生成一个符号字符
+	symbol := symbols[seededRand.Intn(len(symbols))]
+
+	// 2. 构造密码字符数组
+	b := make([]byte, length)
+
+	// 3. 确定符号出现的位置
+	symbolPos := seededRand.Intn(length)
+	b[symbolPos] = symbol
+
+	// 4. 填充其他位置
+	for i := range b {
+		if i == symbolPos {
+			continue
+		}
+		b[i] = lettersAndDigits[seededRand.Intn(len(lettersAndDigits))]
+	}
+
 	return string(b)
 }
 
