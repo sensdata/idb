8b8d94a04b068e65b2460b9a9d1ced55b96e9513
sea <seazhu831@gmail.com>
2025-07-14 14:09:33 +0800

    ref(docs): Update api docs

--- center/docs/docs.go
+++ center/docs/docs.go
@@ -1551,7 +1551,10 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK"
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/model.ComposeCreateResult"
+                        }
                     }
                 }
             },
@@ -1624,7 +1627,10 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK"
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/model.ComposeCreateResult"
+                        }
                     }
                 }
             }
@@ -10338,7 +10344,10 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK"
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/model.LogInfo"
+                        }
                     }
                 }
             }
@@ -10370,7 +10379,10 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK"
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/model.LogInfo"
+                        }
                     }
                 }
             }
