6d0f8538b07e81b3f8c459be008f257ee1a37b2f
sea <seazhu831@gmail.com>
2025-07-23 19:53:23 +0800

    feat(scriptmanager): Add support for querying categories

--- center/core/plugin/shared/scriptmanager.go
+++ center/core/plugin/shared/scriptmanager.go
@@ -10,6 +10,7 @@ import (
 )
 
 type ScriptManager interface {
+	ListCategories(hostID uint64, req model.QueryGitFile) (*model.ScriptList, error)
 	ListScripts(hostID uint64, req model.QueryGitFile) (*model.ScriptList, error)
 }
 
@@ -34,6 +35,35 @@ type ScriptManagerGRPCClient struct {
 }
 
 var _ ScriptManager = (*ScriptManagerGRPCClient)(nil)
+
+func (c *ScriptManagerGRPCClient) ListCategories(hostID uint64, req model.QueryGitFile) (*model.ScriptList, error) {
+	resp, err := c.client.ListCategories(context.Background(), &proto.ListScriptsRequest{
+		HostId:   uint32(hostID),
+		Type:     req.Type,
+		Category: req.Category,
+		Page:     int32(req.Page),
+		PageSize: int32(req.PageSize),
+	})
+	if err != nil {
+		return nil, err
+	}
+	var result model.ScriptList
+	result.Total = resp.Total
+	scripts := make([]*model.ScriptInfo, 0)
+	for _, item := range resp.Items {
+		scripts = append(scripts, &model.ScriptInfo{
+			Source:    item.Source,
+			Name:      item.Name,
+			Extension: item.Extension,
+			Content:   item.Content,
+			Size:      item.Size,
+			ModTime:   item.ModTime,
+			Linked:    item.Linked,
+		})
+	}
+	result.Items = scripts
+	return &result, nil
+}
 
 func (c *ScriptManagerGRPCClient) ListScripts(hostID uint64, req model.QueryGitFile) (*model.ScriptList, error) {
 	resp, err := c.client.ListScripts(context.Background(), &proto.ListScriptsRequest{
@@ -67,6 +97,36 @@ func (c *ScriptManagerGRPCClient) ListScripts(hostID uint64, req model.QueryGitF
 type ScriptManagerGRPCServer struct {
 	Impl ScriptManager
 	*proto.UnimplementedScriptManagerServer
+}
+
+func (s *ScriptManagerGRPCServer) ListCategories(ctx context.Context, req *proto.ListScriptsRequest) (*proto.ListScriptsResponse, error) {
+	var resp proto.ListScriptsResponse
+
+	result, err := s.Impl.ListCategories(
+		uint64(req.HostId),
+		model.QueryGitFile{
+			Type:     req.Type,
+			Category: req.Category,
+			Page:     int(req.Page),
+			PageSize: int(req.PageSize),
+		},
+	)
+	if err != nil {
+		return &resp, err
+	}
+	resp.Total = result.Total
+	for _, item := range result.Items {
+		resp.Items = append(resp.Items, &proto.ScriptInfo{
+			Source:    item.Source,
+			Name:      item.Name,
+			Extension: item.Extension,
+			Content:   item.Content,
+			Size:      item.Size,
+			ModTime:   item.ModTime,
+			Linked:    item.Linked,
+		})
+	}
+	return &resp, nil
 }
 
 func (s *ScriptManagerGRPCServer) ListScripts(ctx context.Context, req *proto.ListScriptsRequest) (*proto.ListScriptsResponse, error) {
