474a41cdf8e47ee85282b0511029f92760540e27
sea <seazhu831@gmail.com>
2025-07-24 13:33:15 +0800

    feat(scriptmanager): Add APIs and models for script management
    - Implemented CRUD APIs for script categories and scripts
    - Added support for script history, diff comparison, and sync
    - Implemented APIs for script execution and log viewing
    - Updated proto files and corresponding gRPC service implementations

--- center/core/api/entry/scripts.go
+++ center/core/api/entry/scripts.go
@@ -1,6 +1,7 @@
 package entry
 
 import (
+	"errors"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,20 @@ import (
 	"github.com/sensdata/idb/core/constant"
 	"github.com/sensdata/idb/core/model"
 )
+
+func getScriptManager() (shared.ScriptManager, error) {
+	// 获取插件客户端
+	plugin, err := plugin.PLUGINSERVER.GetPlugin("scriptmanager")
+	if err != nil {
+		return nil, err
+	}
+	// 类型断言为 gRPC client
+	client, ok := plugin.Stub.(shared.ScriptManager)
+	if !ok {
+		return nil, errors.New("invalid plugin client")
+	}
+	return client, nil
+}
 
 func (b *BaseApi) GetScriptCategories(c *gin.Context) {
 	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
@@ -46,17 +61,10 @@ func (b *BaseApi) GetScriptCategories(c *gin.Context) {
 		PageSize: int(pageSize),
 	}
 
-	// 获取插件客户端
-	plugin, err := plugin.PLUGINSERVER.GetPlugin("scriptmanager")
+	// 获取插件
+	client, err := getScriptManager()
 	if err != nil {
 		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
-		return
-	}
-
-	// 类型断言为 gRPC client
-	client, ok := plugin.Stub.(shared.ScriptManager)
-	if !ok {
-		ErrorWithDetail(c, constant.CodeFailed, "invalid plugin client", nil)
 		return
 	}
 
@@ -68,6 +76,103 @@ func (b *BaseApi) GetScriptCategories(c *gin.Context) {
 	}
 
 	SuccessWithData(c, resp)
+}
+
+func (b *BaseApi) CreateScriptCategory(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.CreateGitCategory
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.CreateCategory(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) UpdateScriptCategory(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.UpdateGitCategory
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.UpdateCategory(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) DeleteScriptCategory(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	scriptType := c.Query("type")
+	if scriptType == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+	if scriptType != "global" && scriptType != "local" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+
+	category := c.Query("category")
+	if category == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid category", err)
+		return
+	}
+
+	req := model.DeleteGitCategory{
+		Type:     scriptType,
+		Category: category,
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.DeleteCategory(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
 }
 
 func (b *BaseApi) GetScriptList(c *gin.Context) {
@@ -112,17 +217,10 @@ func (b *BaseApi) GetScriptList(c *gin.Context) {
 		PageSize: int(pageSize),
 	}
 
-	// 获取插件客户端
-	plugin, err := plugin.PLUGINSERVER.GetPlugin("scriptmanager")
+	// 获取插件
+	client, err := getScriptManager()
 	if err != nil {
 		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
-		return
-	}
-
-	// 类型断言为 gRPC client
-	client, ok := plugin.Stub.(shared.ScriptManager)
-	if !ok {
-		ErrorWithDetail(c, constant.CodeFailed, "invalid plugin client", nil)
 		return
 	}
 
@@ -134,4 +232,426 @@ func (b *BaseApi) GetScriptList(c *gin.Context) {
 	}
 
 	SuccessWithData(c, resp)
+}
+
+func (b *BaseApi) GetScriptDetail(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	scriptType := c.Query("type")
+	if scriptType == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+	if scriptType != "global" && scriptType != "local" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+
+	category := c.Query("category")
+	if category == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid category", err)
+		return
+	}
+
+	name := c.Query("name")
+	if name == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid name", err)
+		return
+	}
+
+	req := model.GetGitFileDetail{
+		Type:     scriptType,
+		Category: category,
+		Name:     name,
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	detail, err := client.GetScriptDetail(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	SuccessWithData(c, detail)
+}
+
+func (b *BaseApi) CreateScript(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.CreateGitFile
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.CreateScript(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) UpdateScript(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.UpdateGitFile
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.UpdateScript(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) DeleteScript(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	scriptType := c.Query("type")
+	if scriptType == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+	if scriptType != "global" && scriptType != "local" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+
+	category := c.Query("category")
+	if category == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid category", err)
+		return
+	}
+
+	name := c.Query("name")
+	if name == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid name", err)
+		return
+	}
+
+	req := model.DeleteGitFile{
+		Type:     scriptType,
+		Category: category,
+		Name:     name,
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.DeleteScript(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) RestoreScript(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.RestoreGitFile
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.RestoreScript(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) GetScriptHistories(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	scriptType := c.Query("type")
+	if scriptType == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+	if scriptType != "global" && scriptType != "local" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+
+	category := c.Query("category")
+	if category == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid category", err)
+		return
+	}
+
+	name := c.Query("name")
+	if name == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid name", err)
+		return
+	}
+
+	page, err := strconv.ParseInt(c.Query("page"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid page", err)
+		return
+	}
+
+	pageSize, err := strconv.ParseInt(c.Query("page_size"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid page_size", err)
+		return
+	}
+
+	req := model.GitFileLog{
+		Type:     scriptType,
+		Category: category,
+		Name:     name,
+		Page:     int(page),
+		PageSize: int(pageSize),
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	detail, err := client.GetScriptHistory(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	SuccessWithData(c, detail)
+}
+
+func (b *BaseApi) GetScriptDiff(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	scriptType := c.Query("type")
+	if scriptType == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+	if scriptType != "global" && scriptType != "local" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid type", err)
+		return
+	}
+
+	category := c.Query("category")
+	if category == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid category", err)
+		return
+	}
+
+	name := c.Query("name")
+	if name == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid name", err)
+		return
+	}
+
+	commitHash := c.Query("commit")
+	if commitHash == "" {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid commit hash", err)
+		return
+	}
+
+	req := model.GitFileDiff{
+		Type:       scriptType,
+		Category:   category,
+		Name:       name,
+		CommitHash: commitHash,
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	detail, err := client.GetScriptDiff(hostID, req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	SuccessWithData(c, detail)
+}
+
+func (b *BaseApi) ScriptSync(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	err = client.ScriptSync(uint(hostID))
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, nil)
+}
+
+func (b *BaseApi) ScriptExec(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	var req model.ExecuteScript
+	if err := CheckBindAndValidate(&req, c); err != nil {
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	result, err := client.ScriptExec(uint(hostID), req)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+	SuccessWithData(c, result)
+}
+
+func (b *BaseApi) GetScriptRunLogs(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	path := c.Query("path")
+	if path == "" {
+		path = "/"
+	}
+
+	page, err := strconv.ParseInt(c.Query("page"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid page", err)
+		return
+	}
+
+	pageSize, err := strconv.ParseInt(c.Query("page_size"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid page_size", err)
+		return
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	result, err := client.GetScriptRunLogs(uint(hostID), path, int(page), int(pageSize))
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	SuccessWithData(c, result)
+}
+
+func (b *BaseApi) GetScriptRunLogDetail(c *gin.Context) {
+	hostID, err := strconv.ParseUint(c.Param("host"), 10, 32)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeErrBadRequest, "Invalid host", err)
+		return
+	}
+
+	path := c.Query("path")
+	if path == "" {
+		path = "/"
+	}
+
+	// 获取插件
+	client, err := getScriptManager()
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	result, err := client.GetScriptRunLogDetail(uint(hostID), path)
+	if err != nil {
+		ErrorWithDetail(c, constant.CodeFailed, err.Error(), nil)
+		return
+	}
+
+	SuccessWithData(c, result)
 }
