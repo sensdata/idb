cf033cfffd25a71e15087ae3a5996f1fa52cb608
larryro <371767072@qq.com>
2025-10-09 14:36:36 +0800

    fix: show heartbeat errors in terminal

--- frontend/src/components/terminal/terminal.vue
+++ frontend/src/components/terminal/terminal.vue
@@ -159,11 +159,7 @@
   // 使用const定义onWsMsgReceived函数
   const onWsMsgReceived = async (ev: MessageEvent) => {
     const msg: ReceiveMsgDo = JSON.parse(ev.data);
-    if (
-      msg.code != null &&
-      msg.code !== 200 &&
-      msg.type !== MsgType.Heartbeat
-    ) {
+    if (msg.code != null && msg.code !== 200) {
       termRef.value?.write(`\x1b[31m${msg.msg}\x1b[m\r\n`);
       if (msg.msg === 'ErrNotInstalled') {
         if (await confirm(t('components.terminal.session.confirmInstall'))) {
